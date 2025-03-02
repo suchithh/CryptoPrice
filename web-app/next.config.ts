353e42cc8ee11407d72b66f840600f20127562a9
suchi <49915548+SuchithH@users.noreply.github.com>
2025-03-02 01:00:14 -0500

    add remote patterns to allow images from remote source

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // use remotePatterns for more control
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
